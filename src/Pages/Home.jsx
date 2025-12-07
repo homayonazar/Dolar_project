5de01d47dc78150df717a6d7a12bbdd6bf9914df
homayonazar <amirrezaabdollahi01@gmail.com>
2025-12-07 15:15:30 +0300

    Header Completed

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,23 +1,59 @@
-import { useState } from 'react'
-import reactLogo from '../assets/react.svg'
+import { useEffect } from "react";
+import LOGO from "../assets/Logo_png.png"
 
 export default function Home() {
 
-  return (
-    <div className=''>
-      <div className="">
-        <div className="main_sec bg-amber-100 rounded-2xl w-[calc(100%-80px)] h-[600px] m-10">
-            <p className='text-stone-500 text-3xl text-center pt-5'>Price of Currencies:</p>
-            <div className="rectangles flex flex-row justify-between gap-5 mx-8">
-                <div className="rec1 bg-gray-200 border-1 rounded-xl border-gray-600 p-20 "></div>
-                <div className="rec2  bg-gray-200 border-1 rounded-xl border-gray-600 p-20 "></div>
-                <div className="rec3 bg-gray-200 border-1 rounded-xl border-gray-600 p-20 "></div>
-                <div className="rec4 bg-gray-200 border-1 rounded-xl border-gray-600 p-20 "></div>
-                <div className="rec4 bg-gray-200 border-1 rounded-xl border-gray-600 p-20 "></div>
-            </div>
+    useEffect(() => {
 
+        const fetchCurrencies = async () => {
+            try {
+                const response = await fetch(
+                    "https://brsapi.ir/Api/Market/Gold_Currency.php?key=BxDJa8e5UrTnhKtdvY7nyK55xMUG5P7J"
+                );
+
+                const data = await response.json();
+
+                console.log("API Response:", data);
+
+            } catch (error) {
+                console.error("API Error:", error);
+            }
+        };
+
+        fetchCurrencies();
+
+    }, []);
+
+    return (
+        <div>
+            <div className="bg-gray-100 h-[1000px] pt-5">
+                <div className="header border border-gray-600 rounded-tl-4xl rounded-br-4xl rounded-sm w-[calc(80%-80px)] h-20 bg-gray-50  mx-auto shadow-2xl">
+                    <div className="headerInside flex flex-row items-center w-full">
+
+                        <nav className="flex flex-row-reverse gap-6 list-none  w-5/6 justify-center ps-30">
+                            <li>خانه</li>
+                            <li>قیمت ارز</li>
+                            <li>درباره ما</li>
+                            <li>تماس با ما</li>
+                        </nav>
+
+                        <div className="logo w-1/6 flex items-center justify-end py-2 pr-6">
+                            <img src={LOGO} alt="" className="w-[60px] " />
+                        </div>
+
+                    </div>
+
+
+
+
+                </div>
+
+                <div className="mainSection ms-[180px] me-[180px] mt-10">
+                    Main section started
+                </div>
+
+
+            </div>
         </div>
-      </div>
-    </div>
-  )
+    );
 }
